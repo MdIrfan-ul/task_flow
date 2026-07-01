c844fee97c00a3f69be3f38de9a1481817fc273d
Irfan <irfan@ozzi.tech>
2026-07-02 01:13:44 +0530

    added passport strategy oauth2.0 google

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
+
+const configService = new ConfigService();
+const APP_PORT = configService.get<number>('APP_PORT') || 8000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(APP_PORT, () => {
+    console.log(`Server is running on port:⚡${APP_PORT}⚡`);
+  });
 }
 bootstrap();
